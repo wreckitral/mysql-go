727342e48c88f7e17cf98da801555d70b58d4245
Defhanaya Sofhiea <defhanayasofhiea@gmail.com>
2024-03-03 09:04:44 +0700

    query for specified id

--- main.go
+++ main.go
@@ -89,3 +89,18 @@ func albumsByArtist(name string) ([]Album, error) {
     return albums, nil
 }
 
+// albumByID queries for the album with the specified ID.
+func albumByID(id int64) (Album, error) {
+    // An album to hold data from the returned row.
+    var alb Album
+
+    row := db.QueryRow("SELECT * FROM album WHERE id = ?", id)
+    if err := row.Scan(&alb.ID, &alb.Title, &alb.Artist, &alb.Price); err != nil {
+        if err == sql.ErrNoRows {
+            return alb, fmt.Errorf("albumsById %d: no such album", id)
+        }
+        return alb, fmt.Errorf("albumsById %d: %v", id, err)
+    }
+    return alb, nil
+}
+
